3a52bae53ed6e98163e92e011f8b0a76a3dea9bd
Kacper Sarzynski <kacpersrzynski@outlook.com>
2022-04-02 16:43:06 +0000

    Add project

--- so.cpp
+++ so.cpp
@@ -4,252 +4,189 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
+#include <vector>
+
+static int PANEL_X = 4;
+static const int PANEL_SLEEP = 50;
+static bool END = false;
+static const int MAIN_WINDOW_X = 2;
+static const int MAIN_WINDOW_Y = 2;
 
 struct Point{
-	int posx, posy;
+	int x, y;
 };
 
-WINDOW *create_newwin(int height, int width, int starty, int startx);
-void destroy_win(WINDOW *local_win);
-void moveWin(WINDOW *win, int startx);
-void clearScreen();
+static const int BALLS = 6;
+static Point balls[BALLS];
+static std::vector<std::thread> threads;
 
-static int position = 4;
-static bool end = false;
-static Point balls[5];
+Point initBall(){
+	Point ball;
+	ball.x = 4 + rand() % (COLS / 2) + COLS / 4;
+	ball.y = 4;
+	return ball;
+};
 
-void printBall(int posy, int posx)
-{
+void printBall(Point ball) {
 	attron(COLOR_PAIR(1));
-	mvprintw(posy-1, posx, "*");
-	mvprintw(posy, posx-1, "***");
-	mvprintw(posy+1, posx, "*");
+	mvprintw(ball.y - 1, ball.x, "*");
+	mvprintw(ball.y, ball.x - 1, "***");
+	mvprintw(ball.y + 1, ball.x, "*");
 	attroff(COLOR_PAIR(1));
 	refresh();
 }
 
-Point initBall()
-{
-	Point ball;
-	ball.posy = 4;
-	ball.posx = 4 + rand()%(COLS/2) + COLS/4;
-	return ball;
-}
-
-void moveBall(int id)
-{
-	while(not end){
+void moveBall(int id) {
+	while(not END) {
 		balls[id] = initBall();
-		int velocity = 20 + 10 * rand()%10;
+		int velocity = 100;
 		std::pair<int, int> direction{0,0};
 		direction.first = rand() % 3 + 1;
 		direction.second = rand() % 7 - 3;
-		int bounce = 4 + rand() % 3;
-		while(not end and bounce >= 0)
-		{
-			int posy, posx;
-			posy = balls[id].posy + direction.first;
-			posx = balls[id].posx + direction.second;
-			if(posy > LINES - 9)
-			{
+		int bounce = 10 + rand() % 3;
+		while(not END and bounce >= 0) {
+			int y, x;
+			y = balls[id].y + direction.first;
+			x = balls[id].x + direction.second;
+			if(y > LINES - 9) {
 				if(bounce == 0)
-				{
 					break;
-				}
-				else
-				{
-					if(posx < position or posx > position + 25)
+				else {
+					if(x < PANEL_X or x > PANEL_X + 25)
 						break;
 					bounce--;
-					posy = 2 * (LINES - 8) - posy;
+					y = 2 * (LINES - 8) - y;
 					direction.first = -direction.first;
 				}
 			}
-			else if(posy < 4)
-			{
+			else if(y < 4) {
 				if(bounce == 0)
-				{
 					break;
-				}
-				else
-				{
+				else {
 					bounce--;
-					posy = 8 - posy;
+					y = 8 - y;
 					direction.first = -direction.first;
 				}
 			}
-			if(posx < 4)
-			{
+			if(x < 4) {
 				if(bounce == 0)
-				{
 					break;
-				}
-				else
-				{
+				else {
 					bounce--;
-					posx = 8 - posx;
+					x = 8 - x;
 					direction.second = -direction.second;
 				}
 			}
-			if(posx > COLS - 5)
-			{
+			if(x > COLS - 5) {
 				if(bounce == 0)
-				{
 					break;
-				}
-				else
-				{
+				else {
 					bounce--;
-					posx = 2 * (COLS - 5) - posx;
+					x = 2 * (COLS - 5) - x;
 					direction.second = -direction.second;
 				}
 			}
-			balls[id].posx = posx;
-			balls[id].posy = posy;
+			balls[id].x = x;
+			balls[id].y = y;
 			std::this_thread::sleep_for(std::chrono::milliseconds(velocity));
 		}
 	}
 }
 
-void panelMovement()
-{
-	bool direction = true; //true = move to the right
-	while(not end)
-	{
-		if(direction)
-		{
-			if(position < COLS - 4 - 25)
-				position++;
-			else
-			{
-				direction = not direction;
-				position--;
-			}
-		}
-		else
-		{
-			if(position > 6)
-				position--;
-			else
-			{
-				direction = not direction;
-				position++;
-			}
-		}
-		std::this_thread::sleep_for(std::chrono::milliseconds(50));
+void moveWindow(WINDOW* window, int startX) {
+	wborder(window, ' ', ' ', ' ',' ',' ',' ',' ',' ');
+	wrefresh(window);
+	mvwin(window, LINES - 7, startX);
+	box(window, 0, 0);
+	wrefresh(window);
+}
+
+WINDOW* createNewWindow(int height, int width, int startY, int startX) {
+	WINDOW* localWindow;
+	localWindow = newwin(height, width, startY, startX);
+	box(localWindow, 0 , 0);
+	wrefresh(localWindow);
+	return localWindow;
+}
+
+void clearScreen() {
+	for(int row = 3; row <= LINES - 8; row++){
+		move(row, 3);
+		for(int col = 3; col <= COLS - 4; col++)
+			addch(' ');
 	}
 }
 
-void printAll(){
-	WINDOW *my_win, *another_win;
-	int startx, starty, width, height;
+void panelMovement() {
+	bool direction = true;
+	while(not END) {
+		if(direction) {
+			if(PANEL_X < COLS - 4 - 25)
+				PANEL_X++;
+			else {
+				direction = not direction;
+				PANEL_X--;
+			}
+		}
+		else {
+			if(PANEL_X > 6)
+				PANEL_X--;
+			else {
+				direction = not direction;
+				PANEL_X++;
+			}
+		}
+		std::this_thread::sleep_for(
+			std::chrono::milliseconds(PANEL_SLEEP));
+	}
+}
+
+void printAll() {
+	WINDOW* mainWindow;
+	WINDOW* panel;
+	int startX, startY, width, height;
 	int ch;
-	balls[0].posx = 15;
-	balls[0].posy = 8;
-	initscr();			/* Start curses mode 		*/
-	cbreak();			/* Line buffering disabled, Pass on
-					 * everty thing to me 		*/
-	keypad(stdscr, TRUE);		/* I need that nifty F1 	*/
+	initscr();
+	cbreak();
+	keypad(stdscr, TRUE);
 	noecho();
 	start_color();
 	init_pair(1, COLOR_BLUE, COLOR_RED);
 	timeout(20);
 	height = LINES - 4;
 	width = COLS - 4;
-	starty = 2;
-	startx = 2;
+	startY = 2;
+	startX = 2;
 	refresh();
-	my_win = newwin(height, width, starty, startx);
-	box(my_win, 0, 0);
-	wrefresh(my_win);
-	another_win = newwin(3, 25, LINES - 7, position);
-	box(another_win, 0, 0);
-	wrefresh(another_win);
-	std::thread t1(panelMovement);
-	std::thread t2(moveBall, 0);
-	std::thread t3(moveBall, 1);
-	std::thread t4(moveBall, 2);
-	std::thread t5(moveBall, 3);
-	std::thread t6(moveBall, 4);
-	while((ch = getch()) != KEY_F(1))
-	{
+	mainWindow = createNewWindow(
+		height,
+		width,
+		MAIN_WINDOW_Y,
+		MAIN_WINDOW_X);
+	panel = createNewWindow(3, 25, LINES - 7, PANEL_X);
+	std::thread panelThread(panelMovement);
+	for(int i = 0; i < BALLS; i++)
+		threads.push_back(std::thread(moveBall, i));
+	while((ch = getch()) != KEY_F(2)) {
 		clearScreen();
-		moveWin(another_win, position);
-		wrefresh(another_win);
-		for(int i = 0; i<5; i++){
-			
-			printBall(balls[i].posy, balls[i].posx);
+		moveWindow(panel, PANEL_X);
+		wrefresh(panel);
+		for(int i = 0; i < BALLS; i++) {
+			printBall(balls[i]);
 			refresh();
 		}
 	}
-	end = true;
-	t1.join();
-	t2.join();
-	t2.join();
-	t3.join();
-	t4.join();
-	t5.join();
-	t6.join();
-	delwin(my_win);
-	delwin(another_win);
+	END = true;
+	for(int i = 0; i < BALLS; i++)
+		threads[i].join();
+	panelThread.join();
+	delwin(mainWindow);
+	delwin(panel);
 }
 
-int main(int argc, char *argv[])
-{	
+int main(int argc, char *argv[]) {
 	srand(time(NULL));
 	printAll();
-
-	
-	endwin();			/* End curses mode		  */
+	endwin();
 	return 0;
-}
-
-void moveWin(WINDOW *win, int startx){
-	wborder(win, ' ', ' ', ' ',' ',' ',' ',' ',' ');
-	wrefresh(win);
-	mvwin(win, LINES - 7, startx);
-	box(win, 0, 0);
-	wrefresh(win);
-}
-
-WINDOW *create_newwin(int height, int width, int starty, int startx)
-{	WINDOW *local_win;
-
-	local_win = newwin(height, width, starty, startx);
-	box(local_win, 0 , 0);		/* 0, 0 gives default characters 
-					 * for the vertical and horizontal
-					 * lines			*/
-	wrefresh(local_win);		/* Show that box 		*/
-
-	return local_win;
-}
-
-void destroy_win(WINDOW *local_win)
-{	
-	/* box(local_win, ' ', ' '); : This won't produce the desired
-	 * result of erasing the window. It will leave it's four corners 
-	 * and so an ugly remnant of window. 
-	 */
-	wborder(local_win, ' ', ' ', ' ',' ',' ',' ',' ',' ');
-	/* The parameters taken are 
-	 * 1. win: the window on which to operate
-	 * 2. ls: character to be used for the left side of the window 
-	 * 3. rs: character to be used for the right side of the window 
-	 * 4. ts: character to be used for the top side of the window 
-	 * 5. bs: character to be used for the bottom side of the window 
-	 * 6. tl: character to be used for the top left corner of the window 
-	 * 7. tr: character to be used for the top right corner of the window 
-	 * 8. bl: character to be used for the bottom left corner of the window 
-	 * 9. br: character to be used for the bottom right corner of the window
-	 */
-	wrefresh(local_win);
-	delwin(local_win);
-}
-
-void clearScreen(){
-	for(int row = 3; row <= LINES - 8; row++){
-		move(row, 3);
-		for(int col = 3; col <= COLS - 4; col++)
-			addch(' ');
-	}
 }
