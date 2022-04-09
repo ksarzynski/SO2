0825d7c49aef2be748a839c06541e8327cbe8eef
Kacper Sarzynski <kacpersarzynski@outlook.com>
2022-04-09 18:24:08 +0000

    remove displaying triangle and add displaying moving panel

--- so.cpp
+++ so.cpp
@@ -1,28 +1,114 @@
 #include <GL/glut.h>
+#include <thread>
+#include <chrono>
 
-void display() {
-    glClearColor( 0, 0, 0, 1 );
-    glClear( GL_COLOR_BUFFER_BIT );
-    glBegin(GL_TRIANGLES);
-    glColor3f( 1, 0, 0 );
-    glVertex2f( -0.8, -0.8 );
-    glColor3f( 0, 1, 0 );
-    glVertex2f( 0.8, -0.8 );
-    glColor3f( 0, 0, 1 );
-    glVertex2f( 0, 0.9 );
-    glEnd();
-    glutSwapBuffers();
+// CONSTANTS //
+const std::chrono::milliseconds SLEEP_TIME
+	= std::chrono::milliseconds { 500 };
+	// WINDOW //
+const int WINDOW_WIDTH = 400;
+const int WINDOW_HEIGHT = 300;
+const int WINDOW_X = 100;
+const int WINDOW_Y = 100;
+const char* WINDOW_TITLE = "SO2 projekt etap 1";
+	// ------ //
+	// PANEL //
+const int PANEL_RED = 255;
+const int PANEL_GREEN = 0;
+const int PANEL_BLUE = 0;
+const GLint panelMaxSpeed = 10;
+	// ----- //
+// --------- //
+
+// GLOBAL VARIABLES //
+bool gameOver = false;
+	// PANEL //
+GLint panelX1 = 100;
+GLint panelY1 = 200;
+GLint panelX2 = 200;
+GLint panelY2 = 250;
+GLint panelSpeed = 5;
+bool panelGoesRight = true;
+	// ----- //
+// ---------------- //
+
+// FUNCTIONS //
+void clear() { glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT); }
+
+	// INIT //
+void initGlut(int argc, char** argv) {
+	glutInit(&argc, argv);
+   	glutInitDisplayMode(GLUT_SINGLE|GLUT_RGB);
 }
 
+void initWindow() {
+	glutInitWindowSize(WINDOW_WIDTH, WINDOW_HEIGHT);
+	glutInitWindowPosition(WINDOW_X, WINDOW_Y);
+	glutCreateWindow(WINDOW_TITLE);
+}
 
-int main( int argc, char** argv ) {
-    glutInit(&argc, argv);
-    glutInitDisplayMode(GLUT_SINGLE);
-    glutInitWindowSize(500,500);
-    glutInitWindowPosition(100,100);
-    glutCreateWindow("GL RGB Triangle");
-    glutDisplayFunc(display);
-    glutMainLoop();
-    return 0;
+void initRestOfGlut() {
+	glClearColor(0, 0, 0, 0);
+	glDisable(GL_DEPTH_TEST);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	glOrtho(0.0, WINDOW_WIDTH - 1, WINDOW_HEIGHT - 1, 0, -1.0, 1.0);
+	glMatrixMode(GL_MODELVIEW);
+}
+
+void init(int argc, char** argv) {
+	initGlut(argc, argv);
+	initWindow();
+	initRestOfGlut();
+}
+	// ---- //
+	// PANEL //
+void movePanel() {
+	GLint newPanelX1 = panelX1;
+	GLint newPanelX2 = panelX2;
+	while(!gameOver) {
+		if(panelGoesRight) {
+			newPanelX1 += panelSpeed;
+			newPanelX2 += panelSpeed;
+			if(newPanelX2 > WINDOW_WIDTH) {
+				panelGoesRight = !panelGoesRight;
+			}
+		}
+		else {
+			newPanelX1 -= panelSpeed;
+			newPanelX2 -= panelSpeed;
+			if(newPanelX1 < 0) {
+				panelGoesRight = !panelGoesRight;
+			}
+		}
+		std::this_thread::sleep_for(SLEEP_TIME);
+	}
+}
+
+void drawPanel() {
+	clear();
+	glLoadIdentity();
+	glBegin(GL_QUADS);
+	glColor3ub(PANEL_RED, PANEL_GREEN, PANEL_BLUE);
+	glVertex2i(panelX1, panelY1);
+	glVertex2i(panelX2, panelY1);
+	glVertex2i(panelX2, panelY2);
+	glVertex2i(panelX1, panelY2);
+	glEnd();
+	glFlush();
+}
+	// ----- //
+
+void drawAll() {
+	glutDisplayFunc(drawPanel);
+}
+// -------- //
+
+int main (int argc, char** argv) {
+	init(argc, argv);
+	std::thread panelThread(movePanel);
+	std::thread drawingThread(drawAll);
+	glutMainLoop();
+	return 0;
 }
 
